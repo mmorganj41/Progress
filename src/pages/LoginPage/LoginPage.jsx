0a7c17af02d63f2ba617dbea5765752152a2bfd9
mmorganj41 <mmorganj41@gmail.com>
2023-01-29 16:27:55 -0500

    basic logic login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,38 +1,75 @@
-import React from 'react';
-import './LoginPage.css';
+import "./LoginPage.css";
+import React, { useState } from "react";
+import {useImmer} from "use-immer";
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import userService from '../../utils/userService';
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
-export default function LoginPage(props){
-   
+export default function LoginPage({handleSignupOrLogin}){
+  const [formState, updateFormState] = useImmer({
+    username: '',
+    password: '',
+  })
+  const [error, setError] = useState(null);
 
-    return (
-        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as='h2' color='teal' textAlign='center'>
-              <Image src='placeholder' /> Log-in to your account
-            </Header>
-            <Form size='large'>
-              <Segment stacked>
-                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-                <Form.Input
-                  fluid
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='Password'
-                  type='password'
-                />
+  const navigate = useNavigate();
 
-                <Button color='teal' fluid size='large'>
-                  Login
-                </Button>
-              </Segment>
-            </Form>
-            <Message>
-              Need an account? <Link to='/signup'>Sign Up</Link>
-            </Message>
-          </Grid.Column>
-        </Grid>
-      );
+  function handleChange(e) {
+    updateFormState(draft => {
+      draft[e.target.name] = e.target.value;
+    })
+  }
+
+  async function handleSubmit() {
+    try {
+      await userService.login(formState);
+      handleSignupOrLogin();
+      navigate('/')
+    } catch(err) {
+      setError(err.message);
+    }
+  }
+
+  return (
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' textAlign='center'>
+          <Image src='placeholder' /> Log In
+        </Header>
+        <Form autoComplete="off" size='large' onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              fluid icon='user'
+              iconPosition='left'
+              placeholder='username'
+              value={formState.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              value={formState.password}
+              onChange={handleChange}
+              type='password'
+            />
+            <Button color='teal' fluid size='large'>
+              Login
+            </Button>
+          </Segment>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+        <Message>
+          New to progress? <Link to='/signup'>Sign up</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  )
 }
+
 
