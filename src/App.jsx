28f7afad7bde3e7bd4bd7e97d5262ce7282a3c84
mmorganj41 <mmorganj41@gmail.com>
2023-01-29 15:42:28 -0500

    Signup route add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import 'semantic-ui-css/semantic.min.css'
 
 import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home Pageeeeeeeeeee</h1>} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
