a270123444035f139ba24a57874b0ea3a67f274d
mmorganj41 <mmorganj41@gmail.com>
2023-01-29 15:49:17 -0500

    login page form create

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './LoginPage.css';
-
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage(props){
    
 
     return (
-      <div>Login Pageeeeee</div>
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='center'>
+              <Image src='placeholder' /> Log-in to your account
+            </Header>
+            <Form size='large'>
+              <Segment stacked>
+                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                />
+
+                <Button color='teal' fluid size='large'>
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              Need an account? <Link to='/signup'>Sign Up</Link>
+            </Message>
+          </Grid.Column>
+        </Grid>
       );
 }
 
