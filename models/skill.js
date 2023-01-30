c6dce92f2f6a8d4eb389f6cbfbf9ba1b5811dcc7
mmorganj41 <mmorganj41@gmail.com>
2023-01-30 09:47:30 -0500

    skill schema

---
+++ models/skill.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const skillSchema = new Schema({
+    name: {type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    experience: {type: Number, min:0, default: 0, required: true},
+    color: {
+        type: String,
+        enum: ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'],
+        default: 'grey'
+    },
+    photoUrl: String,
+    order: {
+        type: Number,
+        default: 0,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Skill', skillSchema);
