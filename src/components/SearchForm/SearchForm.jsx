9b31a256b79e1e42bee5be757b6dc38af8d3c006
mmorganj41 <mmorganj41@gmail.com>
2023-01-30 13:22:22 -0500

    basic searchForm

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Segment, Input, Grid, Divider, Button } from 'semantic-ui-react';
+
+export default function SearchForm() {
+    return(
+        <Segment placeholder>
+            <Grid columns={2} relaxed='very' stackable>
+                <Grid.Column>
+                    <Input
+                        action={{ color: 'teal', content: 'Search' }}
+                        icon='search'
+                        iconPosition='left'
+                        placeholder='Habit'
+                    />
+                </Grid.Column>
+
+                <Grid.Column verticalAlign='middle'>
+                    <Button
+                        color='teal'
+                        content='Create New Skill'
+                        icon='add'
+                        labelPosition='left'
+                    />
+                </Grid.Column>
+            </Grid>
+
+            <Divider vertical>Or</Divider>
+        </Segment>
+)
+}
